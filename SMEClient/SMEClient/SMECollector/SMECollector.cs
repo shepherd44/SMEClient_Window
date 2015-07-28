aa6d0ddca559d780b26a615de5d8899343cf5aa5
shepherd44 <hh1733@naver.com>
2015-07-28 21:00:05 +0900

    save XML form

--- SMEClient/SMEClient/SMECollector/SMECollector.cs
+++ SMEClient/SMEClient/SMECollector/SMECollector.cs
@@ -1,4 +1,43 @@
-﻿using System;
+﻿/**************************************************************
+ * SME Collector
+ * 
+ * XML 구조
+ * <SME>
+ *   <ProjectInformation>
+ *     <Name></Name>
+ *     <Version></Version>
+ *   </ProjectInformation>
+ *   <SystemInformation>
+ *     <PlatformID></PlatformID>
+ *     <ServicePack></ServicePack>
+ *     <OSVersion></OSVersion>
+ *     <CLRVersion></CLRVersion>
+ *     <Is64BitOS></Is64BitOS>
+ *     <IS64BitProcess></Is64BitProcess>
+ *     <PageSize></PageSize>
+ *     <TickCount></TickCount>
+ *   </SystemInformation>
+ *   <ExceptionInformation>
+ *     <Name></Name>
+ *     <Data>
+ *       <Key>Value</Key>
+ *     </Data>
+ *     <Hresult></Hresult>
+ *     <HelpLink></HelpLink>
+ *     <Message></Message>
+ *   </ExceptionInformation>
+ *   <CallStackInformation>
+ *     <Stack>
+ *       <Method></Method>
+ *       <FileName></FileName>
+ *       <Line></Line>
+ *     </Stack>
+ *   </CallStackInformation>
+ * </SME>
+ * 
+ ***************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -70,4 +109,5 @@ namespace SME
         }
         private Thread m_ConsoleThread = null;
     }
+    
 }
